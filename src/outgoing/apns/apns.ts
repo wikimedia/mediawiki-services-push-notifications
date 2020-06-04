116123b3aaf6ca7b7d735671ecbad49eaf88d343
jgiannelos <jgiannelos@wikimedia.org>
2020-06-04 11:53:37 +0300

    Implement endpoint to support Apple Push Notifications
    * Add @parse/node-apn dependency
    * Add swagger spec for APNS push similar to FCM
    * Implement endpoint for APNS
    * Ignore local certs folder
    * Mount certs to local docker-compose based env
    * Initialize apns client on server load
    * Add JSDoc and usage documentation
    
    Bug: T252837
    Change-Id: Iaa256bea61ea9bfadb172f6eb23b22a19386957d

---
+++ src/outgoing/apns/apns.ts
@@ -0,0 +1,78 @@
+import { Provider, ProviderOptions, Notification, Responses } from '@parse/node-apn';
+import { Provider as MockProvider } from '@parse/node-apn/mock';
+import { MultiDeviceMessage } from '../shared/Message';
+
+let apn: Provider;
+
+/**
+ * Get the options to initialize APNS client
+ * If token based auth is defined in config then its preferred
+ * Fallback to cert based auth
+ * @param {*} conf
+ * @return {ProviderOptions}
+ */
+function getOptions(conf): ProviderOptions {
+    let options: ProviderOptions = {};
+
+    if (conf.apns.production) {
+        options = { production: conf.apns.production };
+    }
+
+    if (conf.apns.token) {
+        return {
+            ...options, ...{
+                token: {
+                    key: conf.apns.token.key,
+                    keyId: conf.apns.token.keyId,
+                    teamId: conf.apns.token.teamId
+                }
+            }
+        };
+    }
+
+    return {
+        ...options, ...{
+            pfx: conf.apns.cert
+        }
+    };
+}
+
+/**
+ * Initialize APNS client. Uses mock if configured accordingly.
+ * @export
+ * @param {*} conf App conf
+ */
+export function init(conf) {
+    if (!apn) {
+        if (conf.apns.mock) {
+            apn = new MockProvider();
+        } else {
+            apn = new Provider(getOptions(conf));
+        }
+    }
+}
+
+/**
+ * Send notification to APNS
+ * @export
+ * @param {Logger} logger App logger instance
+ * @param {SingleDeviceMessage} message Notification to be pushed to device
+ */
+export async function sendMessage(logger: Logger, message: MultiDeviceMessage) {
+    let result: Promise<Responses>;
+
+    if (!message.dryRun) {
+        const notification = new Notification();
+        notification.payload = { data: { type: message.messageType } };
+        notification.threadId = message.messageType;
+        result = apn.send(notification, message.deviceTokens);
+    } else {
+        const responses: Responses = {
+            sent: [{ device: 'dryRun' }],
+            failed: []
+        };
+        result = new Promise((resolve) => resolve(responses));
+    }
+
+    return result;
+}
